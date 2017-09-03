efb794ff1e99ee5caf124056a3dbc3d6d968c1ca
PindropAUS <pindrop1995@gmail.com>
2017-09-04 01:47:04 +1000

    Profile Master Test

--- car-sharing-system/car-sharing-system/Views/Admin_Theme/pages/profile.Master.cs
+++ car-sharing-system/car-sharing-system/Views/Admin_Theme/pages/profile.Master.cs
@@ -4,36 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using car_sharing_system.Models;
 
-namespace car_sharing_system.Admin_Theme.pages
+namespace car_sharing_system.Views.Admin_Theme.pages
 {
-    public partial class profile : System.Web.UI.Page
+    public partial class Site1 : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            User newUser = DatabaseReader.userQuerySingle("accountID = '" + User.Identity.Name + "';");
-
-
-            fn.Text = newUser.fname;
-            ln.Text += newUser.lname;
-            licenceNo.Text += newUser.licenceNo;
-            birth.Text += newUser.birth;
-            gender.Text += newUser.gender;
-            phone.Text += newUser.phone;
-            street.Text += newUser.street;
-            suburb.Text += newUser.suburb;
-            postcode.Text += newUser.postcode;
-            territory.Text += newUser.territory;
-            city.Text += newUser.city;
-            country.Text += newUser.country;
-            profileURL.Text += newUser.profileURL;
-
-            if (!Request.IsAuthenticated)
-            {
-                Response.Redirect("~/Views/Admin_Theme/pages/login.aspx");
-            }
         }
     }
 }
