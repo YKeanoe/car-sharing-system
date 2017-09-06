3cdb8b3e4613281c7dc85dddd0e65b57f7d216db
Benjamen Mai <Benjamen2005@hotmail.com>
2017-09-06 15:04:04 +1000

    Fixed database reader

--- car-sharing-system/car-sharing-systemTests/Models/UserModelTests.cs
+++ car-sharing-system/car-sharing-systemTests/Models/UserModelTests.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Rework;
 
 namespace car_sharing_system.Models.Tests
 {
@@ -15,7 +17,8 @@ namespace car_sharing_system.Models.Tests
         public void loginAttemptTestWithAdmin()
         {
             UserModel data = new UserModel();
-            String password = "B59908A396DDEB8456000D031051DA8C786A5DD134901F6644E2727A63965086F904C2E15B7D4736ADF199EF07525D0841628F468518B8CEE565BAC68DCEE2BC";
+            String beforeHash = "admin";
+            String password = (beforeHash + "CarSharing2017").ToSHA(Crypto.SHA_Type.SHA512);
             String UserName = "admin@gmail.com";
             User myData = data.loginAttempt(UserName, password);
             if (myData != null)
@@ -31,9 +34,10 @@ namespace car_sharing_system.Models.Tests
         public void loginAttemptTestWithUser()
         {
             UserModel data = new UserModel();
-            String password = "09E6DA93DF48FFF4A9E21C5788CD55862135BC0A4FD68907F0580320AB3083E8EBC8B8E1A923DCF9D1F910B2E9B208CB69C1C8C7C941E9F5B1CCD113FCC30553";
-            String UserName = "rhoncus.Nullam@egestasSed.org";
-            User myData = data.loginAttempt(UserName, password);
+            string beforeHash = "ZyiXDnElJ";
+            string password = (beforeHash + "CarSharing2017").ToSHA(Crypto.SHA_Type.SHA512);
+            string userName = "rhoncus.Nullam@egestasSed.org";
+            User myData = data.loginAttempt(userName, password);
             if (myData != null)
             {
                 Assert.Pass("Valid User in database");
@@ -47,9 +51,10 @@ namespace car_sharing_system.Models.Tests
         public void loginAttemptNoPassTest()
         {
             UserModel data = new UserModel();
-            String password = "";
-            String UserName = "rhoncus.Nullam@egestasSed.org";
-            User myData = data.loginAttempt(UserName, password);
+            String beforeHash = "";
+            String password = (beforeHash + "CarSharing2017").ToSHA(Crypto.SHA_Type.SHA512);
+            String userName = "rhoncus.Nullam@egestasSed.org";
+            User myData = data.loginAttempt(userName, password);
             if (myData != null)
             {
                 Assert.Fail("No password check failure"); ;
@@ -63,9 +68,10 @@ namespace car_sharing_system.Models.Tests
         public void loginAttemptNoUserTest()
         {
             UserModel data = new UserModel();
-            String password = "09E6DA93DF48FFF4A9E21C5788CD55862135BC0A4FD68907F0580320AB3083E8EBC8B8E1A923DCF9D1F910B2E9B208CB69C1C8C7C941E9F5B1CCD113FCC30553";
-            String UserName = "";
-            User myData = data.loginAttempt(UserName, password);
+            String beforeHash = "ZyiXDnElJ";
+            String password = (beforeHash + "CarSharing2017").ToSHA(Crypto.SHA_Type.SHA512);
+            String userName = "";
+            User myData = data.loginAttempt(userName, password);
             if (myData != null)
             {
                 Assert.Fail("No user check failure"); ;
