b0fcccc03f5396475e0b19142a3de3d9de0d6a97
s3434283 <tonyc77335@hotmail.com>
2017-09-07 00:24:36 +1000

    more user fields

--- car-sharing-system/car-sharing-system/Views/Admin_Theme/pages/register.aspx.cs
+++ car-sharing-system/car-sharing-system/Views/Admin_Theme/pages/register.aspx.cs
@@ -7,17 +7,23 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using car_sharing_system.Models;
 
+
 namespace car_sharing_system.Admin_Theme.pages
 {
     public partial class register : System.Web.UI.Page
     {
-     
 
+        protected User newUser;
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
-     
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            newUser = DatabaseReader.userQueryInsert("accountID = '" + User.Identity.Name + "';");
+
+           
+        }
         }
         }
             
