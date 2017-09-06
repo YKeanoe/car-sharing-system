565d6f033f91fc79818e272e6378c4ed10685c28
PindropAUS <pindrop1995@gmail.com>
2017-09-06 15:06:10 +1000

    Fixed a small bug

--- car-sharing-system/car-sharing-system/Views/Admin_Theme/pages/profile.aspx.cs
+++ car-sharing-system/car-sharing-system/Views/Admin_Theme/pages/profile.aspx.cs
@@ -10,27 +10,13 @@ namespace car_sharing_system.Admin_Theme.pages
 {
     public partial class profile : System.Web.UI.Page
     {
-        protected String fn;
+        protected User newUser;
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            User newUser = DatabaseReader.userQuerySingle("accountID = '" +  User.Identity.Name + "';");
+            newUser = DatabaseReader.userQuerySingle("accountID = '" +  User.Identity.Name + "';");
 
   
-
-            fn = newUser.fname;
-            ln.Text += newUser.lname;
-            licenceNo.Text += newUser.licenceNo;
-            birth.Text += newUser.birth;
-            gender.Text += newUser.gender;
-            phone.Text += newUser.phone;
-            street.Text += newUser.street;
-            suburb.Text += newUser.suburb;
-            postcode.Text += newUser.postcode;
-            territory.Text += newUser.territory;
-            city.Text += newUser.city;
-            country.Text += newUser.country;
-            profileURL.Text += newUser.profileURL;
 
             if (!Request.IsAuthenticated)
             {
