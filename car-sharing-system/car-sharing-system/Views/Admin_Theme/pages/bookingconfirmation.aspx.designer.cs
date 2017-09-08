b081c0723724cfc91ddd45433d7c08d4b399cbb2
rmit-s3323595-yohanes-keanoe <s3323595@student.rmit.edu.au>
2017-09-08 23:38:57 +1000

    small fix

--- car-sharing-system/car-sharing-system/Views/Admin_Theme/pages/bookingconfirmation.aspx.designer.cs
+++ car-sharing-system/car-sharing-system/Views/Admin_Theme/pages/bookingconfirmation.aspx.designer.cs
@@ -7,19 +7,9 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace car_sharing_system.Admin_Theme.pages {
+namespace car_sharing_system.Views.Admin_Theme.pages {
     
     
     public partial class bookingconfirmation {
-        
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
     }
 }
