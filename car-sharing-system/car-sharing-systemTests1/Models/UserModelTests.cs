76e28eb17193d0aea02c258213532dea6af5632b
Benjamen Mai <s3539470@student.rmit.edu.au>
2017-09-20 23:19:41 +1000

    Updated Unit Tests

---
+++ car-sharing-system/car-sharing-systemTests1/Models/UserModelTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_sharing_systemTests1.Models
+{
+    class UserModelTests
+    {
+    }
+}
