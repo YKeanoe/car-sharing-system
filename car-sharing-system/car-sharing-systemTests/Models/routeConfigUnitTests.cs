6b94c4a5658a438b8b9f9a1f8d34baafe8f30c82
Benjamen Mai <s3539470@student.rmit.edu.au>
2017-10-11 21:09:10 +1100

    Updated routing unit tests

--- car-sharing-system/car-sharing-systemTests/Models/routeConfigUnitTests.cs
+++ car-sharing-system/car-sharing-systemTests/Models/routeConfigUnitTests.cs
@@ -30,6 +30,7 @@ namespace car_sharing_systemTests
         }
 
         #region Routing Tests
+
         [TestMethod]
         public void HomePageRoutingTest() // Tests if the home page is using the valid root directory
         {
@@ -42,6 +43,33 @@ namespace car_sharing_systemTests
             // Assert
             Assert.IsNotNull(routeData);
         }
+
+        [TestMethod]
+        public void RegisterRoutingTest()  // Tests if the register page is routed correctly to *.com/dashboard/register
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            car_sharing_system.RouteConfig.RegisterRoutes(routes);
+            moqRequest.Setup(e => e.AppRelativeCurrentExecutionFilePath).Returns("~/dashboard/register");
+            // Act
+            RouteData routeData = routes.GetRouteData(moqContext.Object);
+            // Assert
+            Assert.IsNotNull(routeData);
+        }
+
+        [TestMethod]
+        public void SuccessRegisterRoutingTest()  // Tests if the success register page is routed correctly to *.com/dashboard/congratz
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            car_sharing_system.RouteConfig.RegisterRoutes(routes);
+            moqRequest.Setup(e => e.AppRelativeCurrentExecutionFilePath).Returns("~/dashboard/congratz");
+            // Act
+            RouteData routeData = routes.GetRouteData(moqContext.Object);
+            // Assert
+            Assert.IsNotNull(routeData);
+        }
+
         [TestMethod] 
         public void LoginRoutingTest()  // Tests if the login page is routed correctly to *.com/dashboard/login
         {
@@ -54,6 +82,7 @@ namespace car_sharing_systemTests
             // Assert
             Assert.IsNotNull(routeData);
         }
+
         [TestMethod]
         public void LogoutRoutingTest() // Tests if the logout page is routed correctly to *.com/dashboard/logout
         {
@@ -66,6 +95,7 @@ namespace car_sharing_systemTests
             // Assert
             Assert.IsNotNull(routeData);
         }
+
         [TestMethod]
         public void ProfileRoutingTest() // Tests if the profile page is routed correctly to *.com/dashboard/profile
         {
@@ -78,6 +108,7 @@ namespace car_sharing_systemTests
             // Assert
             Assert.IsNotNull(routeData);
         }
+
         [TestMethod]
         public void BookingRoutingTest() // Tests if the booking page is routed correctly to *.com/dashboard/booking
         {
@@ -90,6 +121,46 @@ namespace car_sharing_systemTests
             // Assert
             Assert.IsNotNull(routeData);
         }
+
+        [TestMethod]
+        public void BookingConfirmationRoutingTest() // Tests if the booking confirmation page is routed correctly to *.com/dashboard/confirmation
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            car_sharing_system.RouteConfig.RegisterRoutes(routes);
+            moqRequest.Setup(e => e.AppRelativeCurrentExecutionFilePath).Returns("~/dashboard/confirmation");
+            // Act
+            RouteData routeData = routes.GetRouteData(moqContext.Object);
+            // Assert
+            Assert.IsNotNull(routeData);
+        }
+
+        [TestMethod]
+        public void BookingReturnRoutingTest() // Tests if the booking return page is routed correctly to *.com/dashboard/return
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            car_sharing_system.RouteConfig.RegisterRoutes(routes);
+            moqRequest.Setup(e => e.AppRelativeCurrentExecutionFilePath).Returns("~/dashboard/return");
+            // Act
+            RouteData routeData = routes.GetRouteData(moqContext.Object);
+            // Assert
+            Assert.IsNotNull(routeData);
+        }
+
+        [TestMethod]
+        public void SuccessBookingRoutingTest() // Tests if the successful booking page is routed correctly to *.com/dashboard/returnsuccess
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            car_sharing_system.RouteConfig.RegisterRoutes(routes);
+            moqRequest.Setup(e => e.AppRelativeCurrentExecutionFilePath).Returns("~/dashboard/returnsuccess");
+            // Act
+            RouteData routeData = routes.GetRouteData(moqContext.Object);
+            // Assert
+            Assert.IsNotNull(routeData);
+        }
+
         [TestMethod]
         public void IssueRoutingTest() // Tests if the issue page is routed correctly to *.com/dashboard/issue
         {
@@ -102,30 +173,33 @@ namespace car_sharing_systemTests
             // Assert
             Assert.IsNotNull(routeData);
         }
+
         [TestMethod]
-        public void RegisterRoutingTest() // Tests if the register page is routed correctly to *.com/dashboard/register
+        public void IssueReportedRoutingTest() // Tests if the issue reported page is routed correctly to *.com/dashboard/issuereported
         {
             // Arrange
             RouteCollection routes = new RouteCollection();
             car_sharing_system.RouteConfig.RegisterRoutes(routes);
-            moqRequest.Setup(e => e.AppRelativeCurrentExecutionFilePath).Returns("~/dashboard/register");
+            moqRequest.Setup(e => e.AppRelativeCurrentExecutionFilePath).Returns("~/dashboard/issuereported");
             // Act
             RouteData routeData = routes.GetRouteData(moqContext.Object);
             // Assert
             Assert.IsNotNull(routeData);
         }
+
         [TestMethod]
-        public void AdminAddCarRoutingTest() // Tests if the admin add car page is routed correctly to *.com/dashboard/admin/addcar
+        public void AdminUsersRoutingTest() // Tests if the admin users page is routed correctly to *.com/dashboard/admin/users
         {
             // Arrange
             RouteCollection routes = new RouteCollection();
             car_sharing_system.RouteConfig.RegisterRoutes(routes);
-            moqRequest.Setup(e => e.AppRelativeCurrentExecutionFilePath).Returns("~/dashboard/admin/addcar");
+            moqRequest.Setup(e => e.AppRelativeCurrentExecutionFilePath).Returns("~/dashboard/admin/users");
             // Act
             RouteData routeData = routes.GetRouteData(moqContext.Object);
             // Assert
             Assert.IsNotNull(routeData);
         }
+
         [TestMethod]
         public void AdminCarsRoutingTest() // Tests if the admin cars page is routed correctly to *.com/dashboard/admin/cars
         {
@@ -138,6 +212,33 @@ namespace car_sharing_systemTests
             // Assert
             Assert.IsNotNull(routeData);
         }
+
+        [TestMethod]
+        public void AdminAddCarRoutingTest() // Tests if the admin add car page is routed correctly to *.com/dashboard/admin/addcar
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            car_sharing_system.RouteConfig.RegisterRoutes(routes);
+            moqRequest.Setup(e => e.AppRelativeCurrentExecutionFilePath).Returns("~/dashboard/admin/addcar");
+            // Act
+            RouteData routeData = routes.GetRouteData(moqContext.Object);
+            // Assert
+            Assert.IsNotNull(routeData);
+        }
+
+        [TestMethod]
+        public void AdminCarViewRoutingTest() // Tests if the admin car view page is routed correctly to *.com/dashboard/admin/car
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            car_sharing_system.RouteConfig.RegisterRoutes(routes);
+            moqRequest.Setup(e => e.AppRelativeCurrentExecutionFilePath).Returns("~/dashboard/admin/car");
+            // Act
+            RouteData routeData = routes.GetRouteData(moqContext.Object);
+            // Assert
+            Assert.IsNotNull(routeData);
+        }
+
         [TestMethod]
         public void AdminIssuesRoutingTest() // Tests if the admin issues page is routed correctly to *.com/dashboard/admin/issues
         {
