4d65f128e74e9a3b0f2abb4b8e8551b5ef637c2e
rmit-s3323595-yohanes-keanoe <s3323595@student.rmit.edu.au>
2017-10-17 20:08:09 +1100

    BookingController api now receive postmethod successfully

--- car-sharing-system/car-sharing-system/Controllers/BookingController.cs
+++ car-sharing-system/car-sharing-system/Controllers/BookingController.cs
@@ -5,6 +5,9 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using car_sharing_system.Models;
+using System.Diagnostics;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json.Linq;
 
 namespace car_sharing_system.Controllers {
 	public class BookingController : ApiController {
@@ -19,10 +22,27 @@ namespace car_sharing_system.Controllers {
 		}
 
 		// POST api/<controller>
-		// Use to grab booking object from post request and set the object
-		// to database.
-		public void Post([FromBody]Booking value) {
-			value.debug();
+		// Use to grab JObject or JSON from post method.
+		// Convert JObject to booking object and add it to database
+		public void Post([FromBody]JObject value) {
+			// Use JObject since post method can't convert 2 object automatically.
+			var newBookJson = value["book"];
+			var locJson = value["location"];
+			Location loc = new Location(
+				Convert.ToDecimal(locJson["lat"]),
+				Convert.ToDecimal(locJson["lng"])
+				);
+			Booking newBook = new Booking(
+				Convert.ToInt32(newBookJson["accountID"]),
+				newBookJson["numberPlate"].ToString(),
+				Convert.ToInt64(newBookJson["bookingDate"].ToString()),
+				Convert.ToInt64(newBookJson["startDate"].ToString()),
+				Convert.ToInt64(newBookJson["estEndDate"].ToString()),
+				loc);
+			// Debugging
+			newBook.debug();
+
+			// TO DO
 		}
 
 		// PUT api/<controller>/5
@@ -31,6 +51,20 @@ namespace car_sharing_system.Controllers {
 
 		// DELETE api/<controller>/5
 		public void Delete(int id) {
+		}
+	}
+
+
+	public class test {
+		String title;
+		String value;
+		public test(String title, String value) {
+			this.title = title;
+			this.value = value;
+		}
+		public void debug() {
+			Debug.WriteLine("title: " + title);
+			Debug.WriteLine("value: " + value);
 		}
 	}
 }
--- car-sharing-system/car-sharing-system/Controllers/UserController.cs
+++ car-sharing-system/car-sharing-system/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Script.Serialization;
+using car_sharing_system.Models;
 
 namespace car_sharing_system.Controllers {
 	public class UserController : ApiController {
